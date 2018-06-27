4cfeb010e4d02abdaa29a1e9a6f5552495dd0b65
soyoungkim9 <soyoung930623@gmail.com>
2018-06-27 21:53:16 +0900

    .

--- projectBackup-challenge/src/main/webapp/html7/header.js
+++ projectBackup-challenge/src/main/webapp/html7/header.js
@@ -1,21 +1,21 @@
 // div#header 태그에 /html/header.html 내용을 삽입한다.
-$.get("/bitcamp-java-project/html7/header.html", (data) => {
+$.get("/projectBackup-challenge/html7/header.html", (data) => {
 	$("#header").html(data);
 	loadLoginUser();
 });
 
 function loadLoginUser() {
 	
-	$.getJSON("/bitcamp-java-project/json/auth/loginUser", (data) => {
+	$.getJSON("/projectBackup-challenge/json/auth/loginUser", (data) => {
 		$("#username").text(data.id);
 		$("#logoutBtn").click((e) => {
 			e.preventDefault(); // 클릭했을 때 원래 하던 일이 있는데 그것을 하지 말라!
-			$.get("/bitcamp-java-project/json/auth/logout", () => {
-				location.href = "/bitcamp-java-project/html7/auth/login.html";
+			$.get("/projectBackup-challenge/json/auth/logout", () => {
+				location.href = "/projectBackup-challenge/html7/auth/login.html";
 			});
 		});
 	}).fail(() => {
-		location.href = "/bitcamp-java-project/html7/auth/login.html";
+		location.href = "/projectBackup-challenge/html7/auth/login.html";
 	});
 	
 }
