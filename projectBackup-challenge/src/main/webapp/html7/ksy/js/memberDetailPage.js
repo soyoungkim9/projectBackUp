2b50f96c3356f50a0bfddc930052acb0639a78e6
soyoungkim9 <soyoung930623@gmail.com>
2018-06-29 21:57:35 +0900

    .

---
+++ projectBackup-challenge/src/main/webapp/html7/ksy/js/memberDetailPage.js
@@ -0,0 +1,57 @@
+/* 장소 div 이벤트 */
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent1");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks1");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+/* 클리상태를 유지하는 이벤트 */
+document.getElementById("defaultOpen1").click();
+
+/* 일정 div 이벤트 */
+function openDay(evt, dayName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks2");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(dayName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen2").click();
+
+/* 본문 따라다니는 navbar 이벤트*/
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function openNav(evt) {
+    var navLinks;
+    navLinks = document.getElementsByClassName("navLink");
+    for (i = 0; i < navLinks.length; i++) {
+        navLinks[i].className = navLinks[i].className.replace(" active", "");
+    }
+    evt.currentTarget.className += " active";
+}
+/* 클리상태를 유지하는 이벤트 */
+document.getElementById("defaultNav").click();
