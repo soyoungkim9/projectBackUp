5de98dc24d3bba2563d2685f464e8214b37c1fd5
soyoungkim9 <soyoung930623@gmail.com>
2018-06-30 18:00:36 +0900

    .

--- projectBackup-challenge/src/main/webapp/html7/ksy/js/memberDetailPage.js
+++ projectBackup-challenge/src/main/webapp/html7/ksy/js/memberDetailPage.js
@@ -55,3 +55,23 @@ function openNav(evt) {
 }
 /* 클리상태를 유지하는 이벤트 */
 document.getElementById("defaultNav").click();
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
