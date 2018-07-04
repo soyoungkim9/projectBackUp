0033b9d16c51ad1a06df2e60caf00f6422e8b067
soyoungkim9 <soyoung930623@gmail.com>
2018-07-04 12:00:43 +0900

    .

--- projectBackup-challenge/src/main/webapp/html7/header/header_white_top.js
+++ projectBackup-challenge/src/main/webapp/html7/header/header_white_top.js
@@ -1,8 +1,3 @@
-//div#header 태그에 /html/header.html 내용을 삽입한다.
-$.get("/challenge-project/html7/header/header_white_top.html", (data) => {
-	$("#header").html(data);
-	loadLoginUser();
-});
 
 
 function expand() {
