d777791f86ce2273ffcaad5be8a526ac5dbe264a
soyoungkim9 <soyoung930623@gmail.com>
2018-07-27 14:40:44 +0900

    .

--- projectBackup-challenge/src/main/webapp/challenge/js/trainer/reportList.js
+++ projectBackup-challenge/src/main/webapp/challenge/js/trainer/reportList.js
@@ -1,79 +1,72 @@
-// 운동일지 보기
-if (location.href.split("?").length > 1) {
-	var pno = location.href.split("?")[1].split("=")[1];
+var plano;
+var pno;
+var defaultPage;
+
+// li-template 트레이너가 관리하는 프로그램 이름 목록
+var liTemplateSrc = $("#li-template").html();
+var templateFn = Handlebars.compile(liTemplateSrc);
+$.ajax(serverRoot + "/json/plan/list/", {
+	dataType: "json",
+    success(data) {
+		$('#programList').html(templateFn({list:data}));
+		defaultPage = $('.active').find('a').attr('data-last');
 		
+		// 운동일지 default page 설정
+		$.ajax(serverRoot + "/json/plan/list/" + defaultPage, {
+			dataType: "json",	
+		    success(data) {
+				 $('#programBox').html(programTemplateFn({
+					 name: data[0].program.name,
+					 startDate: data[0].program.startDate,
+					 endDate: data[0].program.endDate,
+					 list:data}));
+		    },
+		    error() {
+		        window.alert("report.js default page 실행 오류!");
+		    }	
+		});
+	},
+    error() {
+        window.alert("report.js li-template list 실행 오류!");
+    }
+});
+
+// 운동일지 리스트 보기
+var programTemplateSrc = $("#program-template").html();
+var programTemplateFn = Handlebars.compile(programTemplateSrc);
+
+$(document.body).on('click', '.programTab', function(event) {
+	event.preventDefault();
+	pno = $(this).find('a').attr('data-no');
+	
+	// 탭 메뉴 활성화 이벤트
+	if ($('.programTab').find(".active")) {
+		$('.programTab').removeClass("active");
+		$('.programTab').css("color", "#777");
+	} 
+    $(this).addClass("active");
+	
+    // 운동일지 리스트 보기
 	$.ajax(serverRoot + "/json/plan/list/" + pno, {
 		dataType: "json",	
 	    success(data) {
-			$("#programName").append(data[0].program.name);
-	        $("#sdt").append("기간: " + data[0].program.startDate + " ~ ");
-	        $("#edt").append(data[0].program.endDate);
-	        for (var item of data) {
-	        	var li = document.createElement("li");
-	        	li.innerHTML = "<div class='dayContent'>" + 
-	        		"<h4 class='time'>" + "Day"+ item.planTurn + "</h4>" +
-	        		"<pre>" + item.planContent + "</pre>" + "</div>" +
-	        		"<div class='editIcon'>" + 
-	        		"<a onclick='modalView(" + item.no + ")' class='viewNum'><i class='fas fa-plus-circle'></i></a>" 
-	        		+ "</div>";
-	        	planList.appendChild(li);
-	        }
+			 $('#programBox').html(programTemplateFn({
+				 name: data[0].program.name,
+				 startDate: data[0].program.startDate,
+				 endDate: data[0].program.endDate,
+				 list:data}));
 	    },
 	    error() {
-	        window.alert("report.js view list 실행 오류!");
+	        window.alert("report.js programTab list 실행 오류!");
 	    }	
 	});
-}
-
-//li-template
-var liTemplateSrc = $("#li-template").html();
-var templateFn = Handlebars.compile(liTemplateSrc);
-$.getJSON(serverRoot + "/json/plan/list/", (data) => {
- $('#programList').html(templateFn({list:data}));
+	
+	$('#addPlan').css('display','block');	
 });
 
-/* 탭 메뉴 이벤트  나중에*/
-function tabEvent(pno) {
-	var programNo = location.href.split("?")[1].split("=")[1];
-
-	if(programNo == pno) {
-//			if ($(".tabSelect" + {{program.no}}).find(".active")) {
-//				$(".tabSelect" + {{program.no}}).removeClass("active");
-//				$(".tabSelect a" + {{program.no}}).css("color", "#777");
-//			}
-			console.log(pno);
-			console.log(programNo);
-			console.log($("tabSelect" + pno).selector);
-						
-			$("tabSelect" + pno).addClass("active");
-			//$(".active a").css("color", "#fff");
-			//$("tabSelect" + pno).css('background-color', 'red');
-	}
-}
-
-
-
-
-
-
-// 모달 관련 이벤트, 새글 관련 이벤트
+// 모달 관련 이벤트
 var modal = document.getElementById('myModal');
-var btn = document.getElementById("addPlan");
 var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-    $('.modal').css("display", "block");
-    $('#viewForm').css("display", "none");
-    $('#addForm').css("display", "block");
-    // 프로그램 회차 관련
-    var programNo = location.href.split("?")[1].split("=")[1];
-    $.getJSON(serverRoot + "/json/plan/list/" + programNo, (data) => {
-     for (var count = data.length + 1; count <= data[0].program.proTurn; count++) {
-     	var option = $("<option>" + count +"</option>");
-     	$('#mTurn').append(option);
-     }
-    });
-}
 
 span.onclick = function() {
     modal.style.display = "none";
@@ -85,15 +78,41 @@ window.onclick = function(event) {
     }
 }
 
-//원하는 운동일지 확대해서 크게 보기 + 수정!
-function modalView(plno) {
+// 새글 관련 이벤트
+$("#addPlan").click(() => {
+    $('.modal').css("display", "block");
+    $('#viewForm').css("display", "none");
+    $('#addForm').css("display", "block");
+
+    // 프로그램 회차 관련
+    $.ajax(serverRoot + "/json/plan/list/" + pno, {
+    	dataType: "json",
+	    success(data) {
+    	   $("option").remove();
+    	   var count = data.length + 1;
+	       for (count; count <= data[0].program.proTurn; count++) {
+	     	  $('#mTurn').append("<option>" + count +"</option>");
+	       }
+	    },
+	    error() {
+	        window.alert("report.js addPlan 실행 오류!");
+	    }
+    });
+});
+
+//원하는 회차의 운동일지 확대 해서 보기!
+$(document.body).on('click', '.editIcon', function(event) {
+	event.preventDefault();
+	plno = $(this).find('a').attr('data-no');
+	pno = $(this).find('a').attr('data-pno');
+	
 	$('.modal').css("display", "block");
 	$('#addForm').css("display", "none");
 	$('#viewForm').css("display", "block");
 	$('#updatePlan button').css("display", "block");
     
 	$.ajax(serverRoot + "/json/plan/" + plno, {
-		dataType: "json",	
+		dataType: "json",
 	    success(data) {
 			$("#dayFont").html("Day" + data[0].planTurn);
 	        $("#dateFont").html("(" + data[0].planDate + ")");
@@ -101,34 +120,63 @@ function modalView(plno) {
 	        $("#modalViewContent textarea").val(data[0].planContent);
 	    },
 	    error() {
-	        window.alert("report.js view 실행 오류!");
+	        window.alert("report.js editIcon 실행 오류!");
 	    }	
 	});
-	
-	// 수정 버튼 눌렀을 경우 쿼리
-	$("#updatePlanButton").click(() => {
-		var programNo = location.href.split("?")[1].split("=")[1];
-		$.post(serverRoot + "/json/plan/update", {
-			planTitl: $("#modalViewTitle input").val(),
-			planContent: $("#modalViewContent textarea").val(),
-			"no": plno
-		}, () => {
-			location.href = "reportList.html?no=" +programNo;
+});
+
+// 수정 버튼 눌렀을 경우 쿼리
+$("#updatePlanButton").click(() => {
+	$.post(serverRoot + "/json/plan/update/", {
+		planTitl: $("#modalViewTitle input").val(),
+		planContent: $("#modalViewContent textarea").val(),
+		"no": plno
+	}, () => {
+		modal.style.display = "none";
+		
+		/* 업데이트한 상태에서 이전 화면으로 돌아가기 위한 코드임... 뭔가 이상 */
+		$.ajax(serverRoot + "/json/plan/list/" + pno, {
+			dataType: "json",	
+		    success(data) {
+				 $('#programBox').html(programTemplateFn({
+					 name: data[0].program.name,
+					 startDate: data[0].program.startDate,
+					 endDate: data[0].program.endDate,
+					 list:data}));
+		    },
+		    error() {
+		        window.alert("report.js view list 실행 오류!");
+		    }	
 		});
+		
 	});
-}
+});
 
 // 등록 버튼 눌렀을 시
 $("#registerPlan").click(() => {
-	var programNo = location.href.split("?")[1].split("=")[1];
 	$.post(serverRoot + "/json/plan/add", {
 		planTurn: $("#mTurn").val(),
 		planDate: $("#mDate").val(),
 		planTitl: $("#mTitle").val(),
 		planContent: $("#mContent").val(),
-		"program.no": programNo
+		"program.no": pno
 	}, () => {
-		location.href = "reportList.html?no=" +programNo;
+		modal.style.display = "none";
+		
+		/* 업데이트한 상태에서 이전 화면으로 돌아가기 위한 코드임... 뭔가 이상 */
+		$.ajax(serverRoot + "/json/plan/list/" + pno, {
+			dataType: "json",	
+		    success(data) {
+				 $('#programBox').html(programTemplateFn({
+					 name: data[0].program.name,
+					 startDate: data[0].program.startDate,
+					 endDate: data[0].program.endDate,
+					 list:data}));
+		    },
+		    error() {
+		        window.alert("report.js view list 실행 오류!");
+		    }	
+		});
 	});
 });
 
